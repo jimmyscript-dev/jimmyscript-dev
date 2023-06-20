6627e9f4bc519502b2acece87b9f8b16a743be0b
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-06-20 09:53:10 +0800

    update excercise

--- NPM-Excercises/excercise3/hello.js
+++ NPM-Excercises/excercise3/hello.js
@@ -1,2 +1,11 @@
-import { program } from 'commander';
+import { Command } from 'commander';
 
+const program = new Command();
+
+program
+    .option('-n, --name <value>', 'Hello monkey')
+
+program.parse();
+
+const options = program.opts();
+console.log(`Hello ${options.name}`)
