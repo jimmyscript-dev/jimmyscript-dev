2ff88de15a2e4f1db4736c61fee169c3a5a77dd9
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-06-20 17:02:16 +0800

    update excercise

--- Fetch-API-excercise/practice.js
+++ Fetch-API-excercise/practice.js
@@ -3,9 +3,9 @@ import fetch from 'node-fetch';
 const response = fetch('https://www.craigslist.org/about/');
 
 const fetchData = async () => {
-    const response = await fetch('https://www.craigslist.org/about/');
-    const htmlResponse = await response.text();
-    console.log(htmlResponse);
+    const response = await fetch('https://craigslist.org/about/');
+    const hmtlRespo = await response.text();
+    console.log(hmtlRespo);
 };
 
 fetchData();
