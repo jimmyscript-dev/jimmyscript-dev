7bbb55d9cf4f54c329e4420238966e12272983b0
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-07-05 13:39:09 +0800

    additional excercise for recursion

--- Recursion/recursion-ex3.js
+++ Recursion/recursion-ex3.js
@@ -1,3 +1,30 @@
-const flatten = (object) => {
-    
+const ob = {
+    Company: "GeeksforGeeks",
+    Address: "Noida",
+    contact: '+91 - 999999999',
+    mentor: {
+        HTML: "GFG",
+        CSS: "GFG",
+        JavaScript: "GFG"
+    }
 }
+const flatten = (obj) => {
+    let value = {};
+    for (item in obj) {
+        if (typeof obj[item] === 'object' && !Array.isArray(item)) {
+            console.log(obj[item]);
+            const objectFlat = flatten(obj[item])
+            for (innerItem in objectFlat) {
+                value[item + ' ' + innerItem] = objectFlat[innerItem]
+            }
+        } else {
+            value[item] = obj[item];
+        }
+    }
+
+
+    return value;
+}
+
+const output = flatten(ob);
+console.log(output);
