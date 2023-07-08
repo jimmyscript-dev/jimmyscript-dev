1081629a17708add0e4f49529916dce59dbd6987
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-07-08 22:06:34 +0800

    additional example for closure excercise

--- Closures/closures-ex2.js
+++ Closures/closures-ex2.js
@@ -1,23 +1,24 @@
 const createVendingMachine = () => {
     const stock = ['Cola', 'Chips', 'Chocolate', 'Juice', 'Nuts'];
     let coins = 0;
-    const randomItem = Math.floor(Math.random() * stock.length);
-    const randomItemName = stock[randomItem]
-    
+    const randomStuff = Math.floor(Math.random() * stock.length);
+    const randomItem = stock[randomStuff];
     const add25 = () => {
         coins += 25;
         if (coins === 100) {
-            return `You got ${randomItemName}!`;
+            coins = 0;
+            return `You got ${randomItem}!`;
+            
         } else if (coins < 100) {
-            const currentCoin = 100 - coins;
-            return `Insert ${currentCoin} more coins.`;
+            const currentCoins = 100 - coins;
+            return `Insert ${currentCoins} more coins.`;
         }
-        //return coins;
     }
     return add25;
 }
 
 const vendingMachine = createVendingMachine();
+console.log(vendingMachine());
 console.log(vendingMachine());
 console.log(vendingMachine());
 console.log(vendingMachine());
