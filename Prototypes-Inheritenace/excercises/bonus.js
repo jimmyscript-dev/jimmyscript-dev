cd20401f6150cccffb0bc6c5aad1473f64e0d6bf
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-07-11 18:42:59 +0800

    additional excercise for inheritance

---
+++ Prototypes-Inheritenace/excercises/bonus.js
@@ -0,0 +1,8 @@
+function Person(name) {
+    this.name = name;
+}
+
+const celo = Person('Cleo');
+console.log(celo);
+const taylor = new Person('Taylor');
+console.log(taylor);
--- Prototypes-Inheritenace/excercises/ex3.js
+++ Prototypes-Inheritenace/excercises/ex3.js
@@ -9,17 +9,19 @@ const store = {
 
 
 let myStore = {};
-myStore = {...store};
+//myStore = {...store}; //! myStore now Inherits the properties of "store."
 myStore.name = 'My Wonderful Store';
+myStore.__proto__ = store;
 myStore.stock[1].quantity = 300;
-console.log(myStore);
-console.log(myStore.stock)
-console.log(Object.getOwnPropertyNames(myStore)); // [name, stock]
+
+//console.log(Object.getOwnPropertyNames(myStore)); // [name, stock]
 
 
 let yourStore = {};
-yourStore = {... store}
+//yourStore = {... store} //! yourStore now Inherits the properties of "store."
+yourStore.__proto__ = store;
 yourStore.name = 'Not Your Wonderful Store';
-console.log(yourStore.name);
-console.log(yourStore.stock)
-console.log(Object.getOwnPropertyNames(yourStore)); // [name, stock]
+
+console.log(myStore.name, myStore.stock);
+console.log(yourStore.name, yourStore.stock);
+//console.log(Object.getOwnPropertyNames(yourStore)); // [name, stock]
