8b54a6537e820d0b32adedbb39dd5e43b3eb21cc
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-07-17 17:32:40 +0800

    additional example classes

--- OOP/class.js
+++ OOP/class.js
@@ -5,24 +5,27 @@ class createPlayer {
         this.mp = mp;
         this.item = item;
     }
-}
 
-const newPlayer = new createPlayer('Tess Tickles', 100, 100, ['Broken Sword']);
-console.log(Object.getOwnPropertyNames(newPlayer)); // [ 'name', 'hp', 'mp', 'item' ]
-console.log(Object.getOwnPropertyNames(newPlayer.__proto__));
-console.log(newPlayer);
+    speak(phrase) {
+        console.log(`${this.name} says: ${phrase}`)
+    }
 
-function AnotherPlayer(name, hp, mp, item) {
-    this.name = name;
-    this.hp = hp;
-    this.mp = mp;
-    this.item = item;
-}
-
-const anotherPlayer = new AnotherPlayer('Jim', 100, 100, ['Broad Sword']);
-console.log(Object.getOwnPropertyNames(anotherPlayer)); // [ 'name', 'hp', 'mp', 'item' ]
-console.log(Object.getOwnPropertyNames(anotherPlayer.__proto__));
-console.log(anotherPlayer);
+    createConfrim() {
+        console.log('Character Created.')
+    }
+};
 
 const jim = new createPlayer('Jim Russ', 300, 200, ['Divine Rapier']);
+console.log(Object.getOwnPropertyNames(jim))
+console.log(Object.getOwnPropertyNames(jim.__proto__));
 console.log(jim);
+jim.createConfrim();
+
+
+
+const ivo = new createPlayer('Ivo', 200, 100, ['Daedalus']);
+console.log(Object.getOwnPropertyNames(ivo));
+console.log(Object.getOwnPropertyNames(ivo.__proto__));
+console.log(ivo);
+ivo.speak('POTANG INA!')
+ivo.createConfrim();
