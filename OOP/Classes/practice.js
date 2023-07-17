1cd271286b0b45417b397d547e6c4733cd2c8c72
jimmyscript-dev <sarmientojimrose@gmail.com>
2023-07-17 17:42:15 +0800

    additional excercise classes

---
+++ OOP/Classes/practice.js
@@ -0,0 +1,12 @@
+class Animal {
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
+const tiger = new Animal('Tiger', 'Big Cats');
+const lemur = new Animal('Lemur', 'Primates');
+
+console.log(tiger);
+console.log(lemur);
